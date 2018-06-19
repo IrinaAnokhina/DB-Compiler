6ae84f85aaef32003b245f18333a295bfa7adca0
Yuatasia <irina15anohina@gmail.com>
2018-06-19 10:10:42 +0300

    Tags for lexical analyzer and parser

---
+++ Tag.h
@@ -0,0 +1,29 @@
+#ifndef TAG_H
+#define TAG_H
+
+enum Tag
+{
+	AND = 256,
+	BASIC,
+	BREAK,
+	DO,
+	ELSE,
+	EQ,
+	FAlse,
+	GE,
+	ID,
+	IF,
+	INDEX,
+	LE,
+	MINUS,
+	NE,
+	NUM,
+	OR,
+	REAL,
+	TEMP,
+	TRue,
+	WHILE,
+	FOR,
+	CONTINUE
+};
+#endif
